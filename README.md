2bb922ba5387701027fac55f078acd843d861d00
dmccarthy <dmccarthy@tssg.org>
2014-03-11 16:03:39 +0000

    First commit od communications component

---
+++ README.md
@@ -0,0 +1,54 @@
+# communcications
+
+The Cloudlet Platforms cloudlet_api component manages users Cloudlet Platform. It supports create, delete, and read of Cloudlets.
+
+## Getting Started
+Install the module with: `npm install git+ssh://git@gitlab.openi-ict.eu:communications.git`
+
+You will need to install the following through macports or aptitude.
+
+```bash
+sudo port install JsCoverage
+sudo port install phantomjs
+```
+
+or
+
+```bash
+sudo apt-get install JsCoverage
+sudo apt-get install phantomjs
+```
+
+To build the project enter the following commands. Note: npm install is only required the first time the module is built or if a new dependency is added. There are a number of grunt tasks that can be executed including: test, cover, default and jenkins. The jenkins task is executed on the build server, if it doesn't pass then the build will fail.
+
+```bash
+git clone git@gitlab.openi-ict.eu:communications.git
+cd cloudlet_api
+npm install
+
+```
+
+To start the component enter:
+
+```javascript
+node lib/local-runner.js
+```
+
+## Documentation
+
+TODO
+
+
+## Contributors
+
+* Donal McCarthy (dmccarthy@tssg.org)
+
+
+## Release History
+**0.1.0** *(11/3/14 dmccarthy@tssg.org)* First version of the cloudlet API module.
+
+
+## License
+Copyright (c) 2014
+Licensed under the MIT license.
+
---
+++ test/communication_test.js
@@ -0,0 +1,75 @@
+'use strict';
+
+var base_path = require('./basePath.js');
+var commsComp = require(base_path + '../lib/helper.js');
+
+
+
+/*
+  ======== A Handy Little Nodeunit Reference ========
+  https://github.com/caolan/nodeunit
+
+  Test methods:
+    test.expect(numAssertions)
+    test.done()
+  Test assertions:
+    test.ok(value, [message])
+    test.equal(actual, expected, [message])
+    test.notEqual(actual, expected, [message])
+    test.deepEqual(actual, expected, [message])
+    test.notDeepEqual(actual, expected, [message])
+    test.strictEqual(actual, expected, [message])
+    test.notStrictEqual(actual, expected, [message])
+    test.throws(block, [error], [message])
+    test.doesNotThrow(block, [error], [message])
+    test.ifError(value)
+*/
+
+
+
+exports['test'] = {
+   setUp: function(done) {
+      commsComp.init({
+         sms           : {  accountSid : 'AC952cebd3bbdfe9f08ab835c7c9ec7a04', authToken: 'a1a77757930e53545fd3ba29a3e12a67' },
+         email         : { type : 'SMTP', service: 'Gmail', user: 'dmccarthy@gmail.com', pass: 'd0na1McC' },
+         sink          : { spec:'tcp://127.0.0.1:49907', bind:true, type:'pull', id:'communication_id' },
+         logger_params : {
+            'path'     : '/opt/openi/cloudlet_platform/logs/communications',
+            'log_level': 'debug',
+            'as_json'  : true
+         }
+      })
+      done();
+   },
+   'send SMS'   : function(test) {
+
+      var testInput     = [
+         {
+            'type' : 'SMS',
+            'body' : 'test message',
+            'from' : 'TEST',
+            'to'   : '+14108675309'
+         },
+         {
+            'type'      : 'EMAIL',
+            'subject'   : 'test email',
+            'to'        : 'dmccarthy@tssg.org',
+            'text'      : 'Hi There',
+            'html'      : 'Hi <b>There</b>'
+         }
+      ]
+
+      var actual = commsComp.processMessage(testInput);
+
+      //console.log(actual)
+
+      var j = 0;
+      for ( var i =0; i < 10000000; i++){
+         j = Math.sqrt(j + 100 + (5/40) * 11 )
+      }
+      console.log("^^^^^^^^^^^^^^^^^^^^^")
+      console.log(j)
+
+      test.done();
+   }
+}
